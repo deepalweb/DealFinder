7400a71961c135dd5b6d043343fb3a36e4aebb6d
Deepal Rupasinghe <143400712+deepalweb@users.noreply.github.com>
2025-07-14 21:24:22 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  VAPID_PUBLIC_KEY: '',
-  VAPID_PRIVATE_KEY: ''
+    VAPID_PUBLIC_KEY: 'BKttZ8Pqu2kvsWsPj8zpSWcleni4b8mmEFUQl2cH9I7AtJh6fwoDwcGaU9Foc0W1p-fAICPUA7dJoDopPbgN3gQ',
+    VAPID_PRIVATE_KEY: 'zhyqoc6sBlukWGAiMfN9om6xZSDNoSgl37BgyGGR04I',
+    GOOGLE_CLIENT_ID: '485292738463-ogadj8l21hp39ep097l85hdk3gmdvfgh.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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'
 };
